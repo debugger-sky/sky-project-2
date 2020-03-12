2dcb72ee3e9e8f362be7080c38ccf2fed2e91799
newbieskyy <38101791+newbieskyy@users.noreply.github.com>
2020-03-13 01:30:40 +0900

    Add files via upload

---
+++ src/main/java/sky/project/skyproject2/SkyProject2Application.java
@@ -0,0 +1,13 @@
+package sky.project.skyproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/project/skyproject2/SkyProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.project.skyproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
